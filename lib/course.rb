35c94e5661f194d95d442d5bea0e0bfd343e22b8
Michael Fuget <michaelfuget@gmail.com>
2020-12-05 19:42:34 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,6 +1,19 @@
 
 
 class Course 
-  @@all 
+  attr_accessor :title, :schedule, :description
+  @@all = []
   def initialize
+    @@all.push(self)
+  end
+  
+  def self.all 
+    return @@all 
+  end 
+  
+  def self.reset_all 
+    @@all.clear 
+  end 
+  
+  
 end 
